f1e1f560a10b7e265b8deb42f8fb4f2da5c5a37f
fedorkobak <kobfedsur@gmail.com>
2025-12-07 13:44:49 +0300

    update configuration script

--- configuration.sh
+++ configuration.sh
@@ -1,15 +1,7 @@
 jupyter_path="${HOME}/.jupyter"
-user_settings="${jupyter_path}/lab/user-settings/@jupyterlab"
+user_settings="${jupyter_path}/lab/user-settings"
 
 mkdir -p $user_settings
 cp configuration/jupyter_lab_config.py ${jupyter_path}
 
-
-mkdir ${user_settings}/shortcuts-extension/
-cp configuration/shortcuts.jupyterlab-settings ${user_settings}/shortcuts-extension/
-
-mkdir ${user_settings}/filebrowser-extension/
-cp configuration/browser.jupyterlab-settings ${user_settings}/filebrowser-extension/
-
-mkdir ${user_settings}/fileeditor-extension/
-cp configuration/plugin.jupyterlab-settings ${user_settings}/fileeditor-extension/
+cp -r configuration/@jupyterlab ${user_settings}
