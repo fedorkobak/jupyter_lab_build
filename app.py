c404765f08c72743e36788f7196914b7e44da3c6
fedorkobak <kobfedsur@gmail.com>
2025-12-14 15:40:53 +0300

    parameterize host

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.1')
 from gi.repository import Gtk, WebKit2
 
-win = Gtk.Window()
-win.set_default_size(1024, 768)
 
-view = WebKit2.WebView()
-view.load_uri("http://localhost:8888")
+def initialize_window(host: str):
+    win = Gtk.Window()
+    win.set_default_size(1024, 768)
 
-win.add(view)
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
+    view = WebKit2.WebView()
+    view.load_uri(host)
 
-Gtk.main()
+    win.add(view)
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+
+
+if __name__ == "__main__":
+    initialize_window("http://localhost:8888")
+    Gtk.main()
