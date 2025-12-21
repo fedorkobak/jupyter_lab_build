f8d350994de17f6c526fbf5fc9db65b3be41f999
fedorkobak <kobfedsur@gmail.com>
2025-12-21 14:31:19 +0300

    leave only shortcuts configuration globally

--- configuration.sh
+++ configuration.sh
@@ -2,6 +2,5 @@ jupyter_path="${HOME}/.jupyter"
 user_settings="${jupyter_path}/lab/user-settings"
 
 mkdir -p $user_settings
-cp configuration/jupyter_lab_config.py ${jupyter_path}
 
 cp -r configuration/@jupyterlab ${user_settings}
